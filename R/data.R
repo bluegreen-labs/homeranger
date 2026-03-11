8dbad2e5ed525b6280b37ad0a1a6c2f2432314e2
Koen Hufkens <koen.hufkens@gmail.com>
2026-03-11 12:15:46 +0100

    adding reference data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Raster array
+#'
+#' Map data for validation and tests
+#'
+#' @format ## `r`
+"r"
+
+#' Reference modeled track
+#'
+#' Model output from Ranc et al. 2022, for model validation
+#'
+#' @format ## `reference`
+#' A data frame with the following columns:
+#' \describe{
+#'   \item{animal_id}{animal ID}
+#'   \item{r_patch}{row}
+#'   \item{c_patch}{column}
+#'   \item{likelihood}{likelihood}
+#' }
+"reference"
