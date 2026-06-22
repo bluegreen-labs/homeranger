b96719127ef64d26bfcff91bc87ef054c378b0ba
Koen Hufkens <koen.hufkens@gmail.com>
2026-06-22 11:07:37 +0200

    update test runs, realistic parameter ranges

--- analysis/test_parallel_processing.R
+++ analysis/test_parallel_processing.R
@@ -1,30 +1,30 @@
 library(homeranger)
 
-par <- list(
+settings <- list(
   metric = "hr_cost",
   control = list(
     sampler = "DEzs",
     settings = list(
-      iterations = 100
+      iterations = 300
     )
   ),
   par = list(
-    r_l = list(lower=0.0001, upper=1, init = 0.5),
-    w_l = list(lower=0.0001, upper=1, init = 0.5),
+    r_l = list(lower=10, upper=30, init = 15),
+    w_l = list(lower=-1, upper=1, init = 0.5),
     r_d = list(lower=0.0001, upper=1, init = 0.5),
-    w_d = list(lower=-1, upper=-0.0001, init = -0.5),
+    w_d = list(lower=0.0001, upper=1, init = -0.5),
     r_dist = list(lower=0.0001, upper=1, init = 0.5),
     w_dist = list(lower=0.0001, upper=1, init = 0.5),
     step_length_dist = list(lower=0.0001, upper=0.1, init = 0.5),
-    step_length_shape = list(lower=0.3, upper=3, init = 1),
-    threshold_approx_kernel = list(lower=400, upper=402, init = 401),
-    threshold_memory_kernel = list(lower=400, upper=402, init = 401),
+    step_length_shape = list(lower=0.0001, upper=3, init = 1),
+    threshold_approx_kernel = list(lower=6999, upper=7001, init = 7000),
+    threshold_memory_kernel = list(lower=999, upper=1001, init = 1000),
 
     # resource selection coefficients come last
     # these are unnamed
     coef = list(
-      lower = rep(-3, 6),
-      upper = rep(3, 6),
+      lower = rep(-1, 6),
+      upper = rep(1, 6),
       init = rep(0, 6)
     )
   )
@@ -37,7 +37,7 @@ obs <-obs |>
   as.data.frame()
 
 obs <- obs |>
-  dplyr::group_split(animal_id)
+  dplyr::group_split(id)
 
 # Create cluster with n cores
 cl <- parallel::makeCluster(4)
@@ -45,14 +45,14 @@ cl <- parallel::makeCluster(4)
 # export functions / variables
 parallel::clusterExport(
   cl,
-  varlist = list("drivers","par", "hr_fit")
+  varlist = list("drivers","settings", "hr_fit")
 )
 
 split_fit <- function(X){
   hr_fit(
     data = drivers,
     obs = X,
-    par = par,
+    par = settings,
     parallel = FALSE
   )
 }
